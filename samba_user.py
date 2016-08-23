8f2162332678a59c46616fccd1ac25ab16261a11
Marvin Steadfast <marvin@xsteadfastx.org>
2016-08-23 10:08:16 +0200

    getting it ready for adding it to the ansible-modules-extras

--- samba_user.py
+++ samba_user.py
@@ -1,14 +1,31 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# (c) 2016, Marvin Steadfast <marvin@xsteadfastx.org>
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 
 
 DOCUMENTATION = '''
 ---
 module: samba_user
-short_description: Manager samba user accounts.
+short_description: Manager samba user accounts through pdbedit and smbpasswd.
 author: "Marvin Steadfast (@xsteadfastx)"
-requirements:
-    - pdbedit
-    - smbpasswd
+version_added: "2.1"
+requirements: [ pdbedit, smbpasswd ]
 options:
     name:
         description:
@@ -23,7 +40,7 @@ options:
             - Whether the account should exist or not,
               taking action if the state is different from what is stated.
         default: present
-        choices: ["absent", "present"]
+        choices: [ "absent", "present" ]
         required: false
 '''
 
